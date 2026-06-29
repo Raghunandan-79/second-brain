24b4b53a2c25d1ab0d4ac5f9e70ab26be333d4d0
Raghunandan-79 <raghunandansharma1905@gmail.com>
2026-06-29 07:42:04 +0530

    Completed signup endpoint

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,4 +1,7 @@
 import express from "express";
+import { UserModel } from "./db.js";
+import bcrypt from "bcrypt";
+import zod from "zod";
 const app = express();
 
 app.get("/", (req, res) => {
@@ -7,8 +10,50 @@ app.get("/", (req, res) => {
     })
 })
 
-app.post("/api/v1/signup", (req, res) => {
+app.post("/api/v1/signup", async (req, res) => {
+    const requiredBody = zod.object({
+        username: zod.string().min(3).max(100),
+        password: zod.string().min(3).max(30).regex(
+            /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]+$/,
+            {
+                message:
+                    "Password must contain 1 uppercase, 1 lowercase, 1 number and 1 special character"
+            }
+        ),
+    }).strict();
 
+    const parsedDataWithSuccess = requiredBody.safeParse(req.body);
+
+    if (!parsedDataWithSuccess.success) {
+        return res.json({
+            message: "Incorrect form",
+            error: parsedDataWithSuccess.error
+        })
+    }
+
+    const { username, password } = req.body;
+
+    let errorThrown = false;
+    try {
+        const hashedPassword = await bcrypt.hash(password, 5);
+
+        await UserModel.create({
+            username: username,
+            password: password
+        });
+    } catch(e) {
+        res.status(400).json({
+            message: "User with email already exists"
+        });
+
+        errorThrown = true;
+    }   
+
+    if (!errorThrown) {
+        res.json({
+            message: "Signup successful"
+        })
+    }
 })
 
 app.post("/api/v1/signin", (req, res) => {
@@ -32,7 +77,7 @@ app.post("/api/v1/brain/share", (req, res) => {
 })
 
 app.get("/api/v1/brain/:shareLink", (req, res) => {
-    
+
 })
 
 app.listen(3000, () => {
