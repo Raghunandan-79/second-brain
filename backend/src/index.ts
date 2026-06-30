404b92f2242725a1e2a53390809401d9ec5bc26a
Raghunandan-79 <raghunandansharma1905@gmail.com>
2026-06-30 19:12:12 +0530

    Added configurations for deployment

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -8,7 +8,10 @@ import cors from "cors";
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+    origin: process.env.FRONTEND_URL || "http://localhost:5173",
+    credentials: true
+}));
 
 app.get("/", (req, res) => {
     res.json({
@@ -26,8 +29,9 @@ async function main() {
         throw new Error("MONGO_URI is not defined in environment variables");
     }
 
-    app.listen(3000, () => {
-        console.log("App is running on port 3000");
+    const PORT = process.env.PORT || 3000;
+    app.listen(PORT, () => {
+        console.log(`App is running on port ${PORT}`);
     });
 
     try {
