88197b2acbb93e8ddbde9d3803e9c1ef5d0c54cb
Raghunandan-79 <raghunandansharma1905@gmail.com>
2026-06-30 11:08:12 +0530

    Created sidebar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,25 +4,30 @@ import Card from './components/Card'
 import CreateContentModal from './components/CreateContentModal'
 import PlusIcon from './icons/PlusIcon'
 import ShareIcon from './icons/ShareIcon'
+import Sidebar from './components/Sidebar'
 
 const App = () => {
-  const [modalOpen, setModalOpen] = useState(true);
+  const [modalOpen, setModalOpen] = useState(false);
 
   return (
-    <div className='p-4'>
-      <CreateContentModal open={modalOpen} onClose={() => {
-        setModalOpen(false);
-      }} />
-      <div className='flex justify-end gap-4'>
-        <Button variant='secondary' text='Share Brain' startIcon={<ShareIcon size='md' />} />
-        <Button onClick={() => {
-          setModalOpen(true)
-        }} variant='primary' text='Add content' startIcon={<PlusIcon size='lg' />} />
-      </div>
+    <div>
+      <Sidebar />
 
-      <div className='flex gap-4'>
-        <Card type='twitter' link='https://x.com/raghu__79/status/2064031035113996414' title='raghu' />
-        <Card type='youtube' link='https://www.youtube.com/watch?v=EVDblXLpFxY' title='1st year students' />
+      <div className='p-4 ml-72 min-h-screen bg-gray-100 border-gray-100 border-2'>
+        <CreateContentModal open={modalOpen} onClose={() => {
+          setModalOpen(false);
+        }} />
+        <div className='flex justify-end gap-4'>
+          <Button variant='secondary' text='Share Brain' startIcon={<ShareIcon size='md' />} />
+          <Button onClick={() => {
+            setModalOpen(true)
+          }} variant='primary' text='Add content' startIcon={<PlusIcon size='lg' />} />
+        </div>
+
+        <div className='flex gap-4 mt-7'>
+          <Card type='twitter' link='https://x.com/raghu__79/status/2064031035113996414' title='raghu' />
+          <Card type='youtube' link='https://www.youtube.com/watch?v=EVDblXLpFxY' title='1st year students' />
+        </div>
       </div>
     </div>
   )
---
+++ frontend/src/components/Sidebar.tsx
@@ -0,0 +1,9 @@
+const Sidebar = () => {
+  return (
+    <div className="h-screen bg-white border-gray-100 border-r w-72 fixed left-0 top-0">
+        hi there
+    </div>
+  )
+}
+
+export default Sidebar
