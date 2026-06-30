a8374b925b06a7e77c7085cdef7a4f8e6599aad3
Raghunandan-79 <raghunandansharma1905@gmail.com>
2026-06-30 11:40:48 +0530

    Added sidebar items

--- frontend/src/components/Sidebar.tsx
+++ frontend/src/components/Sidebar.tsx
@@ -1,7 +1,22 @@
+import Logo from "../icons/Logo"
+import TwitterIcon from "../icons/TwitterIcon"
+import YoutubeIcon from "../icons/YoutubeIcon"
+import SidebarItem from "./SidebarItem"
+
 const Sidebar = () => {
   return (
     <div className="h-screen bg-white border-gray-100 border-r w-72 fixed left-0 top-0">
-        hi there
+        <div className="flex items-center text-2xl font-bold pt-6 pl-4">
+            <div className="pr-2 text-purple-600">
+                <Logo size="7" />
+            </div>
+            Brainly
+        </div>
+
+        <div className="pt-8 pl-6">
+            <SidebarItem text="X.com" icon={<TwitterIcon size="lg" />} />
+            <SidebarItem text="Youtube" icon={<YoutubeIcon size="lg" />} />
+        </div>
     </div>
   )
 }
--- frontend/src/components/SidebarItem.tsx
+++ frontend/src/components/SidebarItem.tsx
@@ -1,6 +1,20 @@
-const SidebarItem = () => {
+import type { ReactElement } from "react";
+
+const SidebarItem = ({ text, icon }: {
+  text: string;
+  icon: ReactElement;
+}) => {
   return (
-    <div>SidebarItem</div>
+    <div className="flex items-center text-gray-700 py-2 cursor-pointer
+    hover:bg-gray-200 rounded max-w-48 pl-4 transition-all duration-150">
+      <div className="pr-2">
+        {icon}
+      </div>
+
+      <div>
+        {text}
+      </div>
+    </div>
   )
 }
 
